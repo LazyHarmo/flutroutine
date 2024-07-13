e1eef346d1013110f92910dbecd698e7067b101e
LazyHarmo <juespy2546@gmail.com>
2024-07-14 00:00:58 +0700

    feat<app>: updated

---
+++ lib/services/app_router.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../screens/recycle_bin.dart';
+import '../screens/tabs.dart';
+
+class AppRouter {
+  Route? onGenerateRoute(RouteSettings routeSettings) {
+    switch (routeSettings.name) {
+      case RecycleBin.id:
+        return MaterialPageRoute(builder: (context) {
+          return const RecycleBin();
+        });
+      case TabsScreen.id:
+        return MaterialPageRoute(builder: (context) {
+          return const TabsScreen();
+        });
+      default:
+        return null;
+    }
+  }
+}
